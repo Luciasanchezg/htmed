f63cf37c43adaa70d8891b1a52b97aefec018b8a
Luciasanchezg <lucia.sanchezg@outlook.com>
2024-04-04 12:51:42 +0200

    recovering_package

--- R/visualization.R
+++ R/visualization.R
@@ -45,27 +45,16 @@ visual_htmed <- function(
     mediator = 'mediator',
     split = NULL
     ) {
-  checks <- .checks_visual(mediation.form=mediation.form, outcome=outcome)
+  checks <- .checks_visual(mediation.form=mediation.form, outcome=outcome,
+                           pval.column = pval.column, pval = pval,
+                           prop.med = prop.med, acme = acme,
+                           treatment = treatment, mediator = mediator,
+                           split = split)
+
   if (checks) {
-
-    if ( !is.null(pval.column) ) {
-
-      # filtering results giving a p-value (default: 0.05)
-      mediation.form[[outcome]]  <- mediation.form[[outcome]] %>%
-        dplyr::mutate(!!rlang::sym(pval.column) := as.numeric(!!rlang::sym(pval.column)),
-                      !!rlang::sym(pval.column) := replace(!!rlang::sym(pval.column), !!rlang::sym(pval.column) >= pval, NA )) %>%
-
-        dplyr::mutate(!!rlang::sym(prop.med) := as.numeric(!!rlang::sym(prop.med)),
-                      !!rlang::sym(prop.med) := case_when(is.na(!!rlang::sym(pval.column))~NA, TRUE ~ !!rlang::sym(prop.med) )) %>%
-
-        dplyr::mutate(!!rlang::sym(acme) := as.numeric(!!rlang::sym(acme)),
-                      !!rlang::sym(acme) := case_when(is.na(!!rlang::sym(pval.column))~NA, TRUE ~ !!rlang::sym(acme) ))
-
-    }
-    else {
-      pval <- NULL
-      mediation.form[[outcome]] <- mediation.form[[outcome]]
-    }
+    mediation.form[[outcome]] <- .filtering_pval(mediation.form = mediation.form, outcome = outcome,
+                            pval.column = pval.column, pval = pval,
+                            prop.med = prop.med, acme = acme)
 
     if ( !is.null(split) ) {
       # generating a new column called split
@@ -78,7 +67,7 @@ visual_htmed <- function(
         scale_color_gradient2(low = "blue", high = "red", mid = "white", midpoint = 0, na.value = "transparent") +
         ggtitle(paste("Results for outcome:", outcome)) +
         labs(x = "Treatment", y = "Mediator", size="Prop.med", col="Est.med") +
-        scale_x_discrete(guide = guide_axis(angle = 45))
+        scale_x_discrete(guide = guide_axis(angle = 45)) + theme_light()
     }
     else {
       p <- ggplot(data=mediation.form[[outcome]]) +
@@ -86,12 +75,56 @@ visual_htmed <- function(
         scale_color_gradient2(low = "blue", high = "red", mid = "white", midpoint = 0, na.value = "transparent") +
         ggtitle(paste("Results for outcome:", outcome)) +
         labs(x = "Treatment", y = "Mediator", size="Prop.med", col="Est.med") +
-        scale_x_discrete(guide = guide_axis(angle = 45))
+        scale_x_discrete(guide = guide_axis(angle = 45)) + theme_light()
     }
 
     return(p)
   }
 }
+
+
+
+# graph_htmed <- function(
+#     mediation.form,
+#     outcome,
+#     vertex.label.cex=.75,
+#     vertex.size=16,
+#     edge.width = 2,
+#     edge.arrow.size = .5,
+#     ...
+#     ) {
+#   # checks <- .checks_visual(mediation.form=mediation.form, outcome=outcome)
+#   # if (checks) {
+#     tabl <- mediation.form[[outcome]]
+#
+#     # stablising the nodes
+#     nodes <- data.frame(
+#       name = c(unique(tabl[['mediator']]), unique(tabl[['treatment']]))
+#     )
+#
+#     # stablising the edges
+#     relations <- stats::na.omit(tabl) %>%
+#       rename("from" = "treatment", 'to' = 'mediator') %>% dplyr::select(c('from', 'to'))
+#
+#     g <- igraph::graph_from_data_frame(relations, directed=TRUE, vertices=nodes)
+#
+#     # colors for the nodes and the edges
+#     igraph::V(g)[as.character(c(unique(relations[['from']])))]$color <- '#FFA500'
+#     igraph::V(g)[as.character(c(unique(relations[['to']])))]$color <- '#F0FFF0'
+#
+#     '%!in%' <- function(x,y)!('%in%'(x,y))
+#     igraph::plot.igraph(
+#       g,
+#       layout=.layout_in_circles(g, group=igraph::V(g)$name %!in% tabl[['treatment']]),
+#       vertex.label.cex=vertex.label.cex,
+#       vertex.size=vertex.size,
+#       edge.width=edge.width,
+#       edge.arrow.size=edge.arrow.size,
+#       edge.curved=seq(-0.25, 0.25, length = igraph::ecount(g)),
+#       ...
+#     )
+#   # }
+# }
 
 
 #' Graph summary of the high-throughput causal mediation analysis
@@ -110,47 +143,102 @@ visual_htmed <- function(
 graph_htmed <- function(
     mediation.form,
     outcome,
-    vertex.label.cex=.75,
-    vertex.size=16,
-    edge.width = 2,
-    edge.arrow.size = .5,
+    pval.column = NULL,
+    pval = 0.05,
+    prop.med = 'Estimate_Prop._Mediated_(average)',
+    acme = 'Estimate_ACME_(average)',
+    treatment = 'treatment',
+    mediator = 'mediator',
+    split = NULL,
     ...
     ) {
-  checks <- .checks_visual(mediation.form=mediation.form, outcome=outcome)
+  checks <- .checks_visual(mediation.form = mediation.form, outcome = outcome,
+                           pval.column = pval.column, pval = pval,
+                           prop.med = prop.med, acme = acme,
+                           treatment = treatment, mediator = mediator,
+                           split = split)
+
   if (checks) {
-    tabl <- mediation.form[[outcome]]
+    tabl <- .filtering_pval(mediation.form = mediation.form, outcome = outcome,
+                            pval.column = pval.column, pval = pval,
+                            prop.med = prop.med, acme = acme)
 
-    # stablising the nodes
-    nodes <- data.frame(
-      name = c(unique(tabl[['mediator']]), unique(tabl[['treatment']]))
-    )
-
-    # stablising the edges
-    relations <- stats::na.omit(tabl) %>%
-      rename("from" = "treatment", 'to' = 'mediator') %>% dplyr::select(c('from', 'to'))
-
-    g <- igraph::graph_from_data_frame(relations, directed=TRUE, vertices=nodes)
-
-    # colors for the nodes and the edges
-    igraph::V(g)[as.character(c(unique(relations[['from']])))]$color <- '#FFA500'
-    igraph::V(g)[as.character(c(unique(relations[['to']])))]$color <- '#F0FFF0'
-
+    ########
     '%!in%' <- function(x,y)!('%in%'(x,y))
-    igraph::plot.igraph(
-      g,
-      layout=.layout_in_circles(g, group=igraph::V(g)$name %!in% tabl[['treatment']]),
-      vertex.label.cex=vertex.label.cex,
-      vertex.size=vertex.size,
-      edge.width=edge.width,
-      edge.arrow.size=edge.arrow.size,
-      edge.curved=seq(-0.25, 0.25, length = igraph::ecount(g)),
-      ...
-    )
+    ########
+
+    # nodes
+    nodes <- data.frame(
+      name = c(unique(tabl[[mediator]]), unique(tabl[[treatment]]))
+      )
+    n.nodes <- length(unique(c(tabl[[mediator]], tabl[[treatment]])))
+
+    # edges
+    relations <- stats::na.omit(tabl) %>%
+      rename("from" = treatment, 'to' = mediator, 'Prop.med' = prop.med, 'Est.med' = acme) %>%
+      dplyr::select(c('from', 'to', 'Prop.med', 'Est.med', column2split))
+
+    if ( !is.null(split) ) {
+      pList <- list()
+
+      for (i in levels(factor(relations[[split]]))) {
+        relations.i <- relations %>% dplyr::filter(!!rlang::sym(split) == i)
+
+        nodes.i <- nodes %>%
+          dplyr::mutate(vertex.color = case_when(name %in% relations.i$from ~ '#FDA855',
+                                                 name %in% relations.i$to ~ '#0180AB',
+                                                 name %in% tabl[[treatment]] ~ '#FBC495',
+                                                 name %in% tabl[[mediator]] ~ '#BDD6D0')) %>%
+          dplyr::mutate(vertex.color.label = case_when(name %in% unique(c(t(relations.i))) ~ '#000000',
+                                                       name %!in% unique(c(t(relations.i))) ~ '#666666'))
+
+        g <- igraph::graph_from_data_frame(relations.i, directed=TRUE, vertices=nodes.i)
+        coords <-.layout_in_circles(g, group=igraph::V(g)$name %!in% tabl[[treatment]]) %>% as.data.frame()
+        lay <- create_layout(graph = g, layout = 'manual', x = coords$V1, y = coords$V2)
+
+        pList[[i]] <- .graph_ggraph(layout_graph=lay, n.nodes=n.nodes)
+      }
+      return(pList)
+    }
+    else {
+      # node color
+      nodes <- nodes %>%
+        #morado_y_verde
+        # dplyr::mutate(vertex.color = case_when(name %in% relations$from ~ '#7AAF60',
+        #                                        name %in% relations$to ~ '#b272ba',
+        #                                        name %in% tabl[[treatment]] ~ '#d7edcc',
+        #                                        name %in% tabl[[mediator]] ~ '#f7e1fa')) %>%
+        # dplyr::mutate(vertex.color.label = case_when(name %in% unique(c(t(relations))) ~ '#424242',
+        #                                              name %!in% unique(c(t(relations))) ~ '#666666'))
+        #azul_y_naranja
+        dplyr::mutate(vertex.color = case_when(name %in% relations$from ~ '#FDA855',
+                                               name %in% relations$to ~ '#0180AB',
+                                               name %in% tabl[[treatment]] ~ '#FBC495',
+                                               name %in% tabl[[mediator]] ~ '#BDD6D0')) %>%
+        dplyr::mutate(vertex.color.label = case_when(name %in% unique(c(t(relations))) ~ '#000000',
+                                                     name %!in% unique(c(t(relations))) ~ '#666666'))
+      # graph
+      g <- igraph::graph_from_data_frame(relations, directed=TRUE, vertices=nodes)
+      coords <-.layout_in_circles(g, group=igraph::V(g)$name %!in% tabl[[treatment]]) %>% as.data.frame()
+      lay <- create_layout(graph = g, layout = 'manual', x = coords$V1, y = coords$V2)
+
+      return(.graph_ggraph(layout_graph=lay, n.nodes=n.nodes))
+    }
   }
 }
 
 
-.checks_visual <- function(mediation.form, outcome) {
+.checks_visual <- function(
+    mediation.form,
+    outcome,
+    pval.column,
+    pval,
+    prop.med,
+    acme,
+    treatment,
+    mediator,
+    split
+    ) {
   if (!"list" %in% class(mediation.form)) {
     stop("mediation.form is not a list")
   }
@@ -159,33 +247,95 @@ graph_htmed <- function(
     stop("mediation.form is not a list of DataFrames")
   }
 
-  if (!"character" %in% class(outcome)) {
-    stop("outcome is not a character")
+  for (i in c(outcome, treatment, mediator, prop.med, acme)) {
+    if (!"character" %in% class(i)) {
+      stop(paste(i, "is not a character"))
+    }
   }
 
   if (!outcome %in% names(mediation.form)) {
     stop("The outcome is not in mediation.form")
   }
 
-  if ( !all(c('p-value_Prop._Mediated_(average)', 'Estimate_Prop._Mediated_(average)', 'Estimate_ACME_(average)') %in% colnames(mediation.form[[outcome]])) ) {
-    stop("Wrong columns in the outcome chosen")
+  if ( !all(c(prop.med, acme) %in% colnames(mediation.form[[outcome]])) ) {
+    stop("Wrong columns for proportion of mediation and/or ACME in the outcome chosen")
   }
 
-  if ( !all(c('mediator', 'treatment') %in% colnames(mediation.form[[outcome]])) ) {
+  if ( !all(c(mediator, treatment) %in% colnames(mediation.form[[outcome]])) ) {
     stop("treatment and/or mediator columns do not exist")
   }
 
-  x <- mediation.form[[outcome]] %>%
-    filter(!is.na(as.character('p-value_Prop._Mediated_(average)')), !is.na(as.character('Estimate_Prop._Mediated_(average)')), !is.na(as.character('Estimate_ACME_(average)')))
-  if (dim(x)[1] == 0) {
-    stop(paste('None of the mediation models for', outcome, 'presented statistically significant values'))
+  if (!is.null(pval.column)) {
+
+    if (!"character" %in% class(pval.column)) {
+      stop("pval.column is not a character")
+    }
+
+    if (!"numeric" %in% class(pval)) {
+      stop("pval is not a character")
+    }
+
+    if ( !pval.column %in% colnames(mediation.form[[outcome]]) ) {
+      stop("pval.column is not in the dataset")
+    }
+  }
+
+  if (!is.null(split)) {
+
+    if (!"character" %in% class(split)) {
+      stop("split is not a character")
+    }
+
+    if ( !split %in% colnames(mediation.form[[outcome]]) ) {
+      stop("split is not in the dataset")
+    }
   }
 
   return(TRUE)
 }
 
+.filtering_pval <- function(
+    mediation.form,
+    outcome,
+    pval.column,
+    pval,
+    prop.med,
+    acme
+    ) {
+  if ( !is.null(pval.column) ) {
 
-.layout_in_circles <- function(graph, group=1) {
+    message(paste('Results with', pval.column, '<', pval, 'will be filtered out'))
+
+    # filtering results giving a p-value (default: 0.05)
+    mediation.form[[outcome]]  <- mediation.form[[outcome]] %>%
+      dplyr::mutate(!!rlang::sym(pval.column) := as.numeric(!!rlang::sym(pval.column)),
+                    !!rlang::sym(pval.column) := replace(!!rlang::sym(pval.column), !!rlang::sym(pval.column) >= pval, NA )) %>%
+
+      dplyr::mutate(!!rlang::sym(prop.med) := as.numeric(!!rlang::sym(prop.med)),
+                    !!rlang::sym(prop.med) := case_when(is.na(!!rlang::sym(pval.column))~NA, TRUE ~ !!rlang::sym(prop.med) )) %>%
+
+      dplyr::mutate(!!rlang::sym(acme) := as.numeric(!!rlang::sym(acme)),
+                    !!rlang::sym(acme) := case_when(is.na(!!rlang::sym(pval.column))~NA, TRUE ~ !!rlang::sym(acme) ))
+
+    x <- mediation.form[[outcome]] %>%
+      filter(!is.na(as.character(pval.column)))
+    if (dim(x)[1] == 0) {
+      stop(paste('None of the mediation models for', outcome, 'presented statistically significant values, for the p-value chosen'))
+    }
+  }
+  else {
+    message("pval.column argument not provided. Results without filtering data will be displayed")
+    # pval <- NULL
+    mediation.form[[outcome]] <- mediation.form[[outcome]]
+  }
+  return(mediation.form[[outcome]])
+}
+
+
+.layout_in_circles <- function(
+    graph,
+    group=1
+    ) {
   layout <- lapply(split(igraph::V(graph), group), function(x) {
     igraph::layout_in_circle(igraph::induced_subgraph(graph,x))
   })
@@ -195,3 +345,47 @@ graph_htmed <- function(
   x
 }
 
+
+.graph_ggraph <- function(
+    layout_graph,
+    n.nodes,
+    ...
+    ) {
+
+  ggraph(layout_graph) +
+    # edges
+    geom_edge_arc(
+      aes(
+        col = Est.med,
+        width = Prop.med
+      ),
+      # curvature=0.1,
+      strength=0.1,
+      arrow = arrow(length = unit(4, 'mm')),
+      start_cap = circle(1, 'mm'),
+      end_cap = circle(ifelse(n.nodes < 30, 11+n.nodes/10, 7+n.nodes/50), 'mm')
+    ) +
+    scale_edge_width(range = c(.5, 2)) +
+    scale_edge_colour_gradient2(
+      low = "blue",
+      high = "red",
+      mid = "white",
+      midpoint = 0,
+      na.value = "transparent"
+    ) +
+    #nodes
+    geom_node_point(
+      # size = ifelse(n.nodes < 30, 30, 20+300/n.nodes),
+      size = ifelse(n.nodes < 30, 30, 15+200/n.nodes),
+      col = layout_graph$vertex.color
+    ) +
+    geom_node_text(
+      aes(
+        label = name
+      ),
+      col = layout_graph$vertex.color.label,
+      repel = FALSE,
+      size = ifelse(n.nodes < 30, 5, 2.5+30/n.nodes)
+    ) +
+    theme_void()
+}
