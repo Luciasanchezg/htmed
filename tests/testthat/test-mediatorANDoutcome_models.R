781f1c1376a04102d581ecf148f7de1683f885f8
Lucia Sanchez Garcia <lucia.sanchezg@outlook.com>
2026-06-09 21:29:39 +0200

    avoiding check errors

--- tests/testthat/test-mediatorANDoutcome_models.R
+++ tests/testthat/test-mediatorANDoutcome_models.R
@@ -23,13 +23,13 @@ test_that(
     load(file.path(file.tests, 'med_1out.RData'))
 
     med <- mediator_models(
-      column.models='model.m.formula',
-      model.type=lm,
-      data=df,
-      data.models=models_1out,
+      column.models = 'model.m.formula',
+      model.type = lm,
+      data = df,
+      data.models = models_1out,
       model.name = 'model.M',
       ncores = 1
-      ) %>%
+    ) %>%
       dplyr::arrange(model.m.formula)
 
     expect_models_equal(med$model.M, med_1out$model.M)
@@ -47,13 +47,13 @@ test_that(
 
     # generating results
     out <- outcome_models(
-      column.models='model.y.formula',
-      model.type=lm,
-      data=df,
-      data.models=models_1out,
+      column.models = 'model.y.formula',
+      model.type = lm,
+      data = df,
+      data.models = models_1out,
       model.name = 'model.Y',
       ncores = 1
-      ) %>%
+    ) %>%
       dplyr::arrange(model.y.formula)
 
     expect_models_equal(out$model.Y, out_1out$model.Y)
@@ -73,20 +73,20 @@ test_that(
     load(file.path(file.tests, 'medANDout_1out.RData'))
 
     preprocess <- mediator_models(
-      column.models='model.m.formula',
-      model.type=lm,
-      data=df,
-      data.models=models_1out,
+      column.models = 'model.m.formula',
+      model.type = lm,
+      data = df,
+      data.models = models_1out,
       model.name = 'model.M',
       ncores = 1
     ) %>%
       dplyr::arrange(model.m.formula)
 
     preprocess <- outcome_models(
-      column.models='model.y.formula',
-      model.type=lm,
-      data=df,
-      data.models=preprocess,
+      column.models = 'model.y.formula',
+      model.type = lm,
+      data = df,
+      data.models = preprocess,
       model.name = 'model.Y',
       ncores = 1
     ) %>%
@@ -111,31 +111,31 @@ test_that(
     load(file.path(file.tests, 'medANDout_2out.RData'))
 
     preprocess <- mediator_models(
-      column.models='model.m.formula',
-      model.type=lm,
-      data=df,
-      data.models=models_2out,
+      column.models = 'model.m.formula',
+      model.type = lm,
+      data = df,
+      data.models = models_2out,
       model.name = 'model.M',
-      outcome='outcome',
+      outcome = 'outcome',
       ncores = 1
     ) %>%
       dplyr::arrange(model.m.formula)
 
     preprocess <- outcome_models(
-      column.models='model.y.formula',
-      model.type=lm,
-      data=df,
-      data.models=preprocess,
+      column.models = 'model.y.formula',
+      model.type = lm,
+      data = df,
+      data.models = preprocess,
       model.name = 'model.Y',
-      outcome='outcome',
+      outcome = 'outcome',
       ncores = 1
     ) %>%
       dplyr::arrange(model.y.formula)
 
     expect_models_equal(preprocess$model.M, medANDout_2out$model.M)
     expect_models_equal(preprocess$model.Y, medANDout_2out$model.Y)
-  })
-
+  }
+)
 
 
 ## ----------------------------------------------------------------------------
@@ -144,7 +144,12 @@ test_that(
 test_that(
   desc = "data_models() returns a dataframe with correct structure and formulas",
   code = {
-    m <- data_models(outcome = 'MVO', mediator = c('LV.EDV', 'RV.EDV'), treatment = 'LDL')
+    m <- data_models(
+      outcome = 'MVO',
+      mediator = c('LV.EDV', 'RV.EDV'),
+      treatment = 'LDL'
+    )
+
     expect_s3_class(m, "data.frame")
     expect_true(all(c("outcome", "treatment", "mediator", "model.m.formula", "model.y.formula") %in% colnames(m)))
     expect_equal(nrow(m), 2L)  # 1 outcome x 1 treatment x 2 mediators
@@ -153,42 +158,68 @@ test_that(
   }
 )
 
+
 test_that(
   desc = "data_models() returns correct row count for multiple outcomes/treatments/mediators",
   code = {
-    m <- data_models(outcome = c('MVO','Chol'), mediator = c('LV.EDV', 'RV.EDV'), treatment = c('LDL', 'FC'))
+    m <- data_models(
+      outcome = c('MVO', 'Chol'),
+      mediator = c('LV.EDV', 'RV.EDV'),
+      treatment = c('LDL', 'FC')
+    )
+
     expect_equal(nrow(m), 8L)  # 2 x 2 x 2
   }
 )
 
+
 test_that(
   desc = "data_models() accepts custom col_m and col_y names",
   code = {
-    m <- data_models(outcome = 'MVO', mediator = 'LV.EDV', treatment = 'LDL',
-                     col_m = 'form.m', col_y = 'form.y')
+    m <- data_models(
+      outcome = 'MVO',
+      mediator = 'LV.EDV',
+      treatment = 'LDL',
+      col_m = 'form.m',
+      col_y = 'form.y'
+    )
+
     expect_true(all(c("form.m", "form.y") %in% colnames(m)))
     expect_false(any(c("model.m.formula", "model.y.formula") %in% colnames(m)))
   }
 )
 
+
 test_that(
   desc = "data_models() throws errors for invalid inputs",
   code = {
-    expect_error(data_models(outcome = data.frame(x='MVO'), mediator = 'LV.EDV', treatment = 'LDL'),
-                 "Please, provide a vector for the outcome")
-    expect_error(data_models(outcome = 'MVO', mediator = data.frame(x='LV.EDV'), treatment = 'LDL'),
-                 "Please, provide a vector for the mediator")
-    expect_error(data_models(outcome = 'MVO', mediator = 'LV.EDV', treatment = data.frame(x='LDL')),
-                 "Please, provide a vector for the treatment")
-    expect_error(data_models(outcome = 'MVO', mediator = 'LV.EDV', treatment = 'LDL',
-                             col_m = 1, col_y = 'form.y'),
-                 "col_m and col_y must be character strings")
-    expect_error(data_models(outcome = 'MVO', mediator = 'LV.EDV', treatment = 'LDL',
-                             col_m = 'same', col_y = 'same'),
-                 "col_m and col_y must be different")
-    expect_error(data_models(outcome = 'MVO', mediator = 'LV.EDV', treatment = 'LDL',
-                             col_m = 'outcome', col_y = 'form.y'),
-                 "col_m and col_y must not use reserved column names")
+    expect_error(
+      data_models(outcome = data.frame(x = 'MVO'), mediator = 'LV.EDV', treatment = 'LDL'),
+      regexp = "Please, provide a vector for the outcome"
+    )
+    expect_error(
+      data_models(outcome = 'MVO', mediator = data.frame(x = 'LV.EDV'), treatment = 'LDL'),
+      regexp = "Please, provide a vector for the mediator"
+    )
+    expect_error(
+      data_models(outcome = 'MVO', mediator = 'LV.EDV', treatment = data.frame(x = 'LDL')),
+      regexp = "Please, provide a vector for the treatment"
+    )
+    expect_error(
+      data_models(outcome = 'MVO', mediator = 'LV.EDV', treatment = 'LDL',
+                  col_m = 1, col_y = 'form.y'),
+      regexp = "col_m and col_y must be character strings"
+    )
+    expect_error(
+      data_models(outcome = 'MVO', mediator = 'LV.EDV', treatment = 'LDL',
+                  col_m = 'same', col_y = 'same'),
+      regexp = "col_m and col_y must be different"
+    )
+    expect_error(
+      data_models(outcome = 'MVO', mediator = 'LV.EDV', treatment = 'LDL',
+                  col_m = 'outcome', col_y = 'form.y'),
+      regexp = "col_m and col_y must not use reserved column names"
+    )
   }
 )
 
@@ -201,50 +232,50 @@ test_that(
   code = {
     expect_error(
       mediator_models(
-        column.models='model.m.formula',
-        model.type=lm,
-        data=as.matrix(df),
-        data.models=models_1out,
+        column.models = 'model.m.formula',
+        model.type = lm,
+        data = as.matrix(df),
+        data.models = models_1out,
         model.name = 'model.M',
       ),
       regexp = "Your data or data.models are not stored in a DataFrame"
     )
     expect_error(
       mediator_models(
-        column.models='model.m.formula',
-        model.type=lm,
-        data=models_1out,
-        data.models=as.matrix(models_1out),
+        column.models = 'model.m.formula',
+        model.type = lm,
+        data = models_1out,
+        data.models = as.matrix(models_1out),
         model.name = 'model.M',
       ),
       regexp = "Your data or data.models are not stored in a DataFrame"
     )
     expect_error(
       mediator_models(
-        column.models='model.m.formula',
-        model.type='lm',
-        data=models,
-        data.models=models_1out,
+        column.models = 'model.m.formula',
+        model.type = 'lm',
+        data = models,
+        data.models = models_1out,
         model.name = 'model.M',
       ),
       regexp = "model.type is not a function"
     )
     expect_error(
       mediator_models(
-        column.models=lm,
-        model.type=lm,
-        data=models,
-        data.models=models_1out,
+        column.models = lm,
+        model.type = lm,
+        data = models,
+        data.models = models_1out,
         model.name = 'model.M',
       ),
       regexp = "Please, provide the name of the corresponding column as character"
     )
     expect_error(
       mediator_models(
-        column.models='model.m.formula',
-        model.type=lm,
-        data=models_1out,
-        data.models=models_1out,
+        column.models = 'model.m.formula',
+        model.type = lm,
+        data = models_1out,
+        data.models = models_1out,
         model.name = TRUE,
       ),
       regexp = "model.name is not a character"
@@ -258,66 +289,66 @@ test_that(
   code = {
     expect_error(
       mediator_models(
-        column.models='model.x.formula',
-        model.type=lm,
-        data=df,
-        data.models=models_1out,
+        column.models = 'model.x.formula',
+        model.type = lm,
+        data = df,
+        data.models = models_1out,
         model.name = 'model.M',
-        ),
+      ),
       regexp = "Incorrect column name for the models"
     )
     models_subset <- models_1out[1:4,]
     expect_error(
       mediator_models(
-        column.models='mediator',
-        model.type=lm,
-        data=df,
-        data.models=models_subset,
+        column.models = 'mediator',
+        model.type = lm,
+        data = df,
+        data.models = models_subset,
         model.name = 'model.M',
       ),
       regexp = "There are no right formulas in the columns selected"
     )
     expect_error(
       mediator_models(
-        column.models='model.m.formula',
-        model.type=lm,
-        data=df,
-        data.models=models_2out,
+        column.models = 'model.m.formula',
+        model.type = lm,
+        data = df,
+        data.models = models_2out,
         model.name = 'model.M',
       ),
       regexp = "Some models are duplicated"
     )
     expect_error(
       mediator_models(
-        column.models='model.m.formula',
-        model.type=lm,
-        data=df,
-        data.models=models_2out,
+        column.models = 'model.m.formula',
+        model.type = lm,
+        data = df,
+        data.models = models_2out,
         model.name = 'model.M',
-        data.split='split.1'
+        data.split = 'split.1'
       ),
       regexp = "data.split argument is not in data"
     )
     expect_error(
       mediator_models(
-        column.models='model.m.formula',
-        model.type=lm,
-        data=df,
-        data.models=models_2out,
+        column.models = 'model.m.formula',
+        model.type = lm,
+        data = df,
+        data.models = models_2out,
         model.name = 'model.M',
-        data.split=1,
+        data.split = 1,
       ),
       regexp = "data.split is not a character"
     )
     expect_error(
       mediator_models(
-        column.models='model.m.formula',
-        model.type=lm,
-        data=df,
-        data.models=models_2out,
+        column.models = 'model.m.formula',
+        model.type = lm,
+        data = df,
+        data.models = models_2out,
         model.name = 'model.M',
-        data.split=1,
-        ncores='1'
+        data.split = 1,
+        ncores = '1'
       ),
       regexp = "ncores must be numeric if provided"
     )
@@ -330,34 +361,34 @@ test_that(
   code = {
     expect_error(
       mediator_models(
-        column.models='model.m.formula',
-        model.type=lm,
-        data=df,
-        data.models=models_2out,
+        column.models = 'model.m.formula',
+        model.type = lm,
+        data = df,
+        data.models = models_2out,
         model.name = 'model.M',
-        outcome=1
+        outcome = 1
       ),
       regexp = "Please, provide the name of the outcome column as character"
     )
     expect_error(
       mediator_models(
-        column.models='model.m.formula',
-        model.type=lm,
-        data=df,
-        data.models=models_2out,
+        column.models = 'model.m.formula',
+        model.type = lm,
+        data = df,
+        data.models = models_2out,
         model.name = 'model.M',
-        outcome='outcomes'
+        outcome = 'outcomes'
       ),
       regexp = "Incorrect column name for the outcome"
     )
     expect_error(
       mediator_models(
-        column.models='model.m.formula',
-        model.type=lm,
-        data=df,
-        data.models=rbind(models_2out, models_2out),
+        column.models = 'model.m.formula',
+        model.type = lm,
+        data = df,
+        data.models = rbind(models_2out, models_2out),
         model.name = 'model.M',
-        outcome='outcome'
+        outcome = 'outcome'
       ),
       regexp = "Some models are duplicated"
     )
@@ -423,6 +454,7 @@ test_that(
       model.name = 'model.M',
       ncores = 1
     )
+
     expect_error(
       mediator_models(
         column.models = 'model.m.formula',
@@ -434,6 +466,7 @@ test_that(
       ),
       regexp = "already exists"
     )
+
     preprocess2 <- outcome_models(
       column.models = 'model.y.formula',
       model.type = lm,
@@ -442,6 +475,7 @@ test_that(
       model.name = 'model.Y',
       ncores = 1
     )
+
     expect_error(
       outcome_models(
         column.models = 'model.y.formula',
@@ -474,6 +508,7 @@ test_that(
       )},
       regexp = "NA"
     )
+
     expect_true(all(sapply(result$model.M, function(m) inherits(m, "lm"))))
     expect_equal(length(result$model.M[[1]]$residuals), nrow(df) - 1L)
   }
@@ -500,6 +535,7 @@ test_that(
       data.split = 'group',
       ncores = 1
     )
+
     expect_true("group" %in% colnames(result))
     expect_equal(sort(unique(result$group)), c("A", "B"))
     expect_true(all(sapply(result$model.M, function(m) inherits(m, "lm"))))
@@ -513,6 +549,7 @@ test_that(
     df_bin <- df
     df_bin$bin_mediator <- as.integer(df$LV.EDV > 0)
     models_bin <- data_models(outcome = 'MVO', mediator = 'bin_mediator', treatment = 'LDL')
+
     result <- mediator_models(
       column.models = 'model.m.formula',
       model.type = glm,
@@ -522,6 +559,7 @@ test_that(
       ncores = 1,
       family = binomial()
     )
+
     expect_true(inherits(result$model.M[[1]], "glm"))
     expect_equal(result$model.M[[1]]$family$family, "binomial")
   }
@@ -532,6 +570,7 @@ test_that(
   desc = "outcome_models() returns a character string in model column when model fitting fails",
   code = {
     bad_models <- data_models(outcome = 'nonexistent', mediator = 'LV.EDV', treatment = 'LDL')
+
     result <- outcome_models(
       column.models = 'model.y.formula',
       model.type = lm,
@@ -540,6 +579,7 @@ test_that(
       model.name = 'model.Y',
       ncores = 1
     )
+
     expect_true(is.character(result$model.Y[[1]]))
     expect_match(result$model.Y[[1]], "Error message")
   }
@@ -553,6 +593,7 @@ test_that(
   desc = "mediator_models() returns a character string in model column when model fitting fails",
   code = {
     bad_models <- data_models(outcome = 'outcome.1', mediator = 'nonexistent', treatment = 'treatment.1')
+
     result <- mediator_models(
       column.models = 'model.m.formula',
       model.type = lm,
@@ -561,6 +602,7 @@ test_that(
       model.name = 'model.M',
       ncores = 1
     )
+
     expect_true(is.character(result$model.M[[1]]))
     expect_match(result$model.M[[1]], "Error message")
   }
@@ -579,11 +621,11 @@ test_that(
       model.name = 'model.M',
       ncores = 1
     )
+
     has_warning <- sapply(result$model.M, function(m) !is.null(m$warningsModel))
     expect_true(any(has_warning))
     expect_true(all(sapply(result$model.M[has_warning], function(m) inherits(m, "lm"))))
   }
 )
-
 
 
